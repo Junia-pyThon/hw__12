9ec9b1dbe22ea13948d26d5761690d67781adef8
Evgenii <evrenbi4@gmail.com>
2022-07-30 01:42:59 +0300

    step 2

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask, request, render_template, send_from_directory
 # from functions import ...
+from main.views import main_blueprint
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def page_index():
-    pass
+app.register_blueprint(main_blueprint)
 
 
 @app.route("/list")
--- functions.py
+++ functions.py
@@ -1,1 +1,13 @@
 import json
+
+def load_posts() -> list[dict]:
+    with open('posts.json', 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+
+def get_posts_by_word(word: str) -> list[dict]:
+    result = []
+    for post in load_posts():
+        if word.lower() in post['content'].lower():
+            result.append(post)
+    return result
